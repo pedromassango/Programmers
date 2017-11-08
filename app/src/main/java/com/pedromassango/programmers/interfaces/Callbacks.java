5ba2c3d273b274d4cebdef14fe859d330e12e77b
pedromassango <peter3fashion@gmail.com>
2017-11-08 10:34:46 +0100

    Generics callbacks added

--- app/src/main/java/com/pedromassango/programmers/interfaces/Callbacks.java
+++ app/src/main/java/com/pedromassango/programmers/interfaces/Callbacks.java
@@ -4,8 +4,23 @@ package com.pedromassango.programmers.interfaces;
  * Created by pedromassango on 11/8/17.
  */
 
+import java.util.List;
+
 /**
- * Contains generic callbacks
+ * Contains generic callbacks.
  */
 public class Callbacks {
+
+    // For single result
+    public interface IResultCallback<T> {
+        void onSuccess(T result);
+        void onDataUnavailable();
+    }
+
+    // For multiple itens results
+    public interface IResultsCallback<T> {
+        void onSuccess(List<T> results);
+        void onDataUnavailable();
+    }
+
 }
