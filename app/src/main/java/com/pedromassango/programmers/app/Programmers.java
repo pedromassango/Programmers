58bd0fc9981c6c6dc1460ce5df937d31f182b542
pedromassango <peter3fashion@gmail.com>
2017-11-10 01:03:49 +0100

    Added Acra in Application class

--- app/src/main/java/com/pedromassango/programmers/app/Programmers.java
+++ app/src/main/java/com/pedromassango/programmers/app/Programmers.java
@@ -5,10 +5,22 @@ import android.support.multidex.MultiDexApplication;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
+import com.pedromassango.programmers.R;
+
+import org.acra.ReportField;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
+/// Coloque seu email para receber os erros.
+@ReportsCrashes(formKey = "", // will not be used
+        mailTo = "pedromassango.developer@gmail.com",
+        customReportContent = { ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME, ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL, ReportField.CUSTOM_DATA, ReportField.STACK_TRACE, ReportField.LOGCAT },
+        mode = ReportingInteractionMode.TOAST,
+        resToastText = R.string.crash_toast_text
+)
 /**
  * Created by Pedro Massango on 03-03-2017 11:02.
  */
@@ -27,7 +39,7 @@ public class Programmers extends MultiDexApplication {
         Realm.init( this);
 
         RealmConfiguration config = new RealmConfiguration.Builder()
-                .name("appname_database.realm")
+                .name("programmers-data.realm")
                 .schemaVersion(111)
                // .migration(new MyMigration())
                 .build();
